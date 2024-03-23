f19e3b2e412252d14a734963a220f0c5aa8a6e8f
Hugo A'Violet <hbaviolet@hotmail.co.uk>
2024-03-23 12:55:02 +0000

    styling output

--- main.py
+++ main.py
@@ -12,17 +12,22 @@ response = requests.get(url, headers=headers)
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    live_matches = soup.find('div', class_="liveMatches")
-    #upcoming_matches = soup.find('div', class_='upcomingMatchesAll')
+    upcoming_matches = soup.find('div', class_='upcomingMatchesAll')
 
-    if live_matches:
-        print(live_matches.get_text())
+    if upcoming_matches:
+        matches_text = upcoming_matches.get_text().strip()
+        matches_list = [match.strip() for match in matches_text.split('\n\n') if
+                        match.strip()]  # Split matches and remove extra spaces
+
+        for match in matches_list:
+            match_lines = match.split('\n')
+            for i, line in enumerate(match_lines):
+                if ':' in line:  # Check if the line contains a colon
+                    if i == 0:  # Add a space above the time if it's not the first line
+                        print()
+                if "Odds" not in line:  # Check if the line does not contain "Odds"
+                    print(line.strip().center(80))
     else:
         print("Div not found")
-
-    #if upcoming_matches:
-      #  print(upcoming_matches.get_text())
-    #else:
-     #   print("Div not found")
 else:
     print("Failed to retrieve webpage:", response.status_code)
